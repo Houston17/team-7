7625fb6155e75647404c75bc0b22eb917450dcef
Ram Luitel <rluitel@iastate.edu>
2017-10-14 08:29:52 -0500

    version 0.0 of product

---
+++ SearchWebApp/js/eventList.js
@@ -0,0 +1,8 @@
+var events = [
+  {'Date': new Date(2017, 9, 25), 'Title': 'Help Mr.Smith to move to new Apartment. Need 10 helping hands'},
+  {'Date': new Date(2017, 9, 20), 'Title': 'Office help. Need 5 volunteer'},
+  {'Date': new Date(2017, 9, 27), 'Title': 'Six volunteers needed to pick donation cloths'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
