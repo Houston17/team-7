cc0805a938ce383e24b408be363a83e775e660d8
Ram Luitel <rluitel@iastate.edu>
2017-10-14 02:56:28 -0500

    Google Map added

--- SearchWebApp/js/custom.js
+++ SearchWebApp/js/custom.js
@@ -7,3 +7,14 @@ $(function(){
     new WOW().init();
     
 });
+
+    
+function myMap() {
+            var mapProp= {
+                center:new google.maps.LatLng(51.508742,-0.120850),
+                zoom:5,
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            }
+
+    
